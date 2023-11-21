605278dd8bf2b8d9a36876707fc862b0a8ced9ed
MichalVasulka <142238782+MichalVasulka@users.noreply.github.com>
2023-11-21 11:56:35 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/endpoint1')
+def endpoint1():
+    result = {'message': 'Hello from endpoint1'}
+    return jsonify(result)
+
+@app.route('/api/endpoint2')
+def endpoint2():
+    result = {'message': 'Hello from endpoint2'}
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
